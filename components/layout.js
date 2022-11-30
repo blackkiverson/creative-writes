86acbaea4b2daea4624582884dbc853cbbad3db2
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-11-30 17:19:58 +0100

    ...

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,18 @@
 import Nav from './Nav';
+import Link from 'next/link';
 
 export default function Layout({children}){
-    return(
-        <div className='mx-6 md:max-w-2xl md:mx-auto font-poppins'>
-            <Nav/>
-            <main>{children}</main>
+    return (
+      <div className="mx-6 md:max-w-2xl md:mx-auto font-poppins">
+        <Nav />
+        <main>{children}</main>
+        <div className="flex absolute bottom-10">
+          <Link href="https://myportfolio-blue-psi.vercel.app/">
+            <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-full text-sm">
+              Meet the Developer
+            </button>
+          </Link>
         </div>
-    )
+      </div>
+    );
 }
