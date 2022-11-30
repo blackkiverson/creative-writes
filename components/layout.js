57dd3c13c2ab1b53306815943d748ec707416827
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2022-11-30 19:28:16 +0100

    positioning the button at the bottom screen fixed

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout({children}){
       <div className="mx-6 md:max-w-2xl md:mx-auto font-poppins">
         <Nav />
         <main>{children}</main>
-        <div className="flex absolute bottom-10">
+        <div className="flex fixed bottom-10">
           <Link href="https://myportfolio-blue-psi.vercel.app/">
             <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-full text-sm">
               Meet the Developer
